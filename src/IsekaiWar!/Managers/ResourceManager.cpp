d210bbc70ef46d6d3990fdf5bc38a445f0090650
ALVES-Ethan <ealves@gaming.tech>
2024-12-16 16:49:01 +0100

    Added audio to ResourceManager

--- src/IsekaiWar!/Managers/ResourceManager.cpp
+++ src/IsekaiWar!/Managers/ResourceManager.cpp
@@ -9,25 +9,47 @@
 #pragma endregion
 
 ResourceManager::ResourceManager() {
+	std::string path = OS::GetExecutablePath();
 
-	std::string path = OS::GetExecutablePath() + "/resourcepacks/";
+	LoadTextures(path);
+	LoadSounds(path);
+}
 
-	std::vector<std::string> assets = OS::GetFilesInDirectory(path, ".png");
+void ResourceManager::LoadTextures(std::string path) {
+	std::vector<std::string> assets = OS::GetFilesInDirectory(path + "/resourcepacks/", ".png");
 
 	for (const std::string& asset : assets) {
 		std::string name = asset.substr(asset.find_last_of("/") + 1);
 		name = name.substr(0, name.find_last_of("."));
 		AddTexture(name, asset);
-    }
+	}
+}
+void ResourceManager::LoadSounds(std::string path) {
+	std::vector<std::string> assets = OS::GetFilesInDirectory(path + "/resourcepacks/", ".wav");
+
+	for (const std::string& asset : assets) {
+		std::string name = asset.substr(asset.find_last_of("/") + 1);
+		name = name.substr(0, name.find_last_of("."));
+		AddSound(name, asset);
+	}
 }
 
 void ResourceManager::AddTexture(std::string name, std::string path) {
 	bool success = textures[name].loadFromFile(path);
 	Debug::Assert(success, "Could not add the texture to the resources");
 }
-
 sf::Texture* ResourceManager::GetTexture(std::string name) {
 	bool success = textures.contains(name);
 	Debug::Assert(success, "Could not retreive requested resource");
 	return &textures[name];
+}
+
+void ResourceManager::AddSound(std::string name, std::string path) {
+	bool success = sounds[name].loadFromFile(path);
+	Debug::Assert(success, "Could not add the sound to the resources");
+}
+sf::SoundBuffer* ResourceManager::GetSound(std::string name) {
+	bool success = sounds.contains(name);
+	Debug::Assert(success, "Could not retreive requested resource");
+	return &sounds[name];
 }
--- src/IsekaiWar!/Managers/ResourceManager.h
+++ src/IsekaiWar!/Managers/ResourceManager.h
@@ -3,21 +3,26 @@
 #pragma region External Dependencies
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <string>
 #include <map>
 
 #pragma endregion
 
-const std::string path = "../../../src/IsekaiWar!/Assets/";
-
 class ResourceManager {
 private:
 	std::map<std::string, sf::Texture> textures;
+	std::map<std::string, sf::SoundBuffer> sounds;
 	ResourceManager();
 
+	void LoadTextures(std::string path);
+	void LoadSounds(std::string path);
 public:
 	void AddTexture(std::string name, std::string path);
 	sf::Texture* GetTexture(std::string name);
+
+	void AddSound(std::string name, std::string path);
+	sf::SoundBuffer* GetSound(std::string name);
 
 	friend class Managers;
 };
