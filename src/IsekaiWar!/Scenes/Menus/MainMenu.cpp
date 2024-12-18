176e52bc6bf9a4112f722c51fc5415a45a49388a
Jay Morrington <ealves@gaming.tech>
2024-12-18 07:19:17 +0100

    MyBad

--- src/IsekaiWar!/Scenes/Menus/MainMenu.cpp
+++ src/IsekaiWar!/Scenes/Menus/MainMenu.cpp
@@ -17,26 +17,42 @@
 void MainMenu::Load() {
 	std::cout << "Main Menu Loaded" << std::endl;
 
-	Spritesheet* spritesheet = new Spritesheet(Managers::GetInstance()->ResourceManager->GetTexture("button"), 1, 4);
+	Spritesheet* play_spritesheet = new Spritesheet(Managers::GetInstance()->ResourceManager->GetTexture("button"), 1, 4);
+	sf::Text* play_text = new sf::Text();
+	play_text->setFont(*Managers::GetInstance()->ResourceManager->GetFont("font"));
+	play_text->setString("Play");
+	play_text->setCharacterSize(30);
+	Button* play_button = new Button(play_text, play_spritesheet);
+	play_button->SetScale(0.25f, 0.25f);
+	play_button->setPosition(1920 / 2, 300);
+	elements.push_back(play_button);
 
-	sf::Text* text = new sf::Text();
-	text->setFont(*Managers::GetInstance()->ResourceManager->GetFont("font"));
-	text->setString("Go fuck yourself");
-	text->setCharacterSize(30);
+	Spritesheet* settings_spritesheet = new Spritesheet(Managers::GetInstance()->ResourceManager->GetTexture("button"), 1, 4);
+	sf::Text* settings_text = new sf::Text();
+	settings_text->setFont(*Managers::GetInstance()->ResourceManager->GetFont("font"));
+	settings_text->setString("Settings");
+	settings_text->setCharacterSize(30);
+	Button* settings_button = new Button(settings_text, settings_spritesheet);
+	settings_button->SetScale(0.25f, 0.25f);
+	settings_button->setPosition(1920 / 2, 400);
+	elements.push_back(settings_button);
 
-	Button* button = new Button(text, spritesheet);
-	button->SetScale(0.25f, 0.25f);
-	button->setPosition(400, 300);
-
-	elements.push_back(button);
+	Spritesheet* quit_spritesheet = new Spritesheet(Managers::GetInstance()->ResourceManager->GetTexture("button"), 1, 4);
+	sf::Text* quit_text = new sf::Text();
+	quit_text->setFont(*Managers::GetInstance()->ResourceManager->GetFont("font"));
+	quit_text->setString("Quit");
+	quit_text->setCharacterSize(30);
+	Button* quit_button = new Button(quit_text, quit_spritesheet);
+	quit_button->SetScale(0.25f, 0.25f);
+	quit_button->setPosition(1920/2, 500);
+	elements.push_back(quit_button);
 }
 
 void MainMenu::UpdateLogic(float dt) {
 	MenuScene::UpdateLogic(dt);
 
-	Button* button = (Button*)elements[0];
-
-	if (button->IsPressed()) {
-		button->Disable();
-	}
+	//Button* button = (Button*)elements[0];
+	//if (button->IsPressed()) {
+	//	button->Disable();
+	//}
 }
