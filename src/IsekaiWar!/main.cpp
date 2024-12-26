9eb5301bdd13bd5e4afe8384d7ee0bf074d180a6
Ethan ALVES <animating.du.38@gmail.com>
2024-12-27 00:18:10 +0100

    Removed console for release configuration
    Changed the solution generator config to remove the console in release configuration.

--- src/IsekaiWar!/main.cpp
+++ src/IsekaiWar!/main.cpp
@@ -1,8 +1,18 @@
 #include "Managers.h"
 
+#ifdef _DEBUG
 int main() {
 	srand(time(NULL));
 	Managers::GetInstance()->GameManager->Run();
 	delete Managers::GetInstance();
 	return 0;
 }
+#else
+#include <Windows.h>
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+	srand(time(NULL));
+	Managers::GetInstance()->GameManager->Run();
+	delete Managers::GetInstance();
+	return 0;
+}
+#endif
